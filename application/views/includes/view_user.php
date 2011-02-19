c46cc94fbdc8d4e182f25e570f97d4126cfcc1af
Marth Santos <marthjefferson@yahoo.com>
2011-02-19 16:53:28 +0800

    added unix to human for timestamp data

--- application/views/includes/view_user.php
+++ application/views/includes/view_user.php
@@ -5,8 +5,8 @@
 <p>Location: <?php echo $user['location']?></p>
 <p>Company: <?php echo $user['company']?></p>
 <p>Contact #: <?php echo $user['phone']?></p>
-<p>Created on: <?php echo $user['created_on']?></p>
-<p>Last login: <?php echo $user['last_login']?></p>
+<p>Created on: <?php echo unix_to_human($user['created_on'])?></p>
+<p>Last login: <?php echo unix_to_human($user['last_login'])?></p>
 <br>
   <a href="#" onclick="history.go(-1); return false;">Back</a>
 </div>
