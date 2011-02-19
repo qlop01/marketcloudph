655dd6b22ac389fcd0462f78c8998cb58cc01f1e
Lucky Sebastian <luckyrps@yahoo.com>
2011-02-19 17:30:25 +0800

    created message_model

---
+++ application/controllers/message.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Message extends Controller {
+	public function __construct() {
+		parent::Controller();
+	    $this->load->model('message_model'); // loads message_model	
+	}	
+	
+	public function index() {
+	
+	}
+	
+	public function new() {
+	
+	}
+	
+	public function create() {
+		
+	}
+	
+	public function view() {
+	
+	}
+	
+	public function delete() {
+		
+	}
+}
+	
